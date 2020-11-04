f2a64f3779f4471ace54bd1cfc506c4804f78d86
Leonardo Losoviz <leo@getpop.org>
2020-11-04 13:35:13 +0800

    Fixed phpstan level 8

--- src/TranslationAPI.php
+++ src/TranslationAPI.php
@@ -8,7 +8,7 @@ use function __;
 
 class TranslationAPI implements \PoP\Translation\TranslationAPIInterface
 {
-    public function __($text, $domain = 'default'): string
+    public function __(string $text, string $domain = 'default'): string
     {
         return __($text, $domain);
     }
